7a6805c1d92bd8495ab37e8cca8db5d530c2d8c1
Hardik <hardikjiio25@gmail.com>
2025-07-04 17:25:27 +0530

    Week 3 Creating and Configuring a Maven Project

---
+++ Week3_Spring_Core_Maven/03_Creating_&_configuring_Maven_Project/Code/src/main/java/com/library/App.java
@@ -0,0 +1,10 @@
+package com.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
